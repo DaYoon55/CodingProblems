44c07afda673a8a0064466c3bc5920104a7b180c
DaYoon55 <Dayoon55@icloud.com>
2018-10-03 08:16:03 +0900

    Update count_string_decodings.cpp

--- count_string_decodings.cpp
+++ count_string_decodings.cpp
@@ -25,16 +25,14 @@ void make_nums(vector<string> &nums){
 	}
 }
 
-//recursive
 void num_ways(string input_st, vector<string> &nums, int &ans){
 	int found = false;
 	
-	//if input_st.len() == 1 or 0: base case
 	if (input_st.size() == 0)
 		ans += 1;
 	else{
 		for(const auto& v:nums){
-			//comparison at head
+			
 			found = true;
 			int v_size = v.size();
 			for (int i=0; i<v_size; ++i){
@@ -43,11 +41,11 @@ void num_ways(string input_st, vector<string> &nums, int &ans){
 					break;
 				}
 			}
-			//yes
+			
 			if (found == true){
 				num_ways(input_st.substr(v_size), nums, ans);
 			}
-			//no
+			
 			else{
 			}
 		}
