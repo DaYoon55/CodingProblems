766d17b3016c4d80e679e4620e3c3fc543b43d72
DaYoon55 <Dayoon55@icloud.com>
2018-10-12 22:06:09 +0900

    Add files via upload

---
+++ palindrome.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+bool is_pal(string& st, string::iterator begin, string::iterator end){
+
+	bool output = false;
+	if (begin == end) output = true;
+	else{
+		if (*begin == *end) {
+			if (begin+1 <= end-1) output = is_pal(st, begin+1, end-1);
+			else output = true;
+		}
+		else output = false;
+	}
+	return output;
+}
+
+int main(){
+
+	string homie;
+	cin >> homie;
+
+	cout << is_pal(homie, homie.begin(), homie.end()-1) << endl;
+
+}
