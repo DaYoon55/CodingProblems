35c4008c94b5225e3e0cc217851b47e67dec1a28
DaYoon55 <Dayoon55@icloud.com>
2018-10-12 22:08:07 +0900

    Update palindrome.cpp

--- palindrome.cpp
+++ palindrome.cpp
@@ -2,13 +2,13 @@
 
 using namespace std;
 
-bool is_pal(string& st, string::iterator begin, string::iterator end){
+bool is_pal(string::iterator begin, string::iterator end){
 
 	bool output = false;
 	if (begin == end) output = true;
 	else{
 		if (*begin == *end) {
-			if (begin+1 <= end-1) output = is_pal(st, begin+1, end-1);
+			if (begin+1 <= end-1) output = is_pal(begin+1, end-1);
 			else output = true;
 		}
 		else output = false;
@@ -21,6 +21,6 @@ int main(){
 	string homie;
 	cin >> homie;
 
-	cout << is_pal(homie, homie.begin(), homie.end()-1) << endl;
+	cout << is_pal(homie.begin(), homie.end()-1) << endl;
 
 }
